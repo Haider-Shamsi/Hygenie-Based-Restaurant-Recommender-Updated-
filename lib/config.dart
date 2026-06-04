8ae79a746a2c02de4676f63d90f763337e839b43
Abdullah Gilani <abdullahgilani215@gmail.com>
2026-06-05 02:07:56 +0500

    prod url changes in config.dart

--- lib/config.dart
+++ lib/config.dart
@@ -1,15 +1,10 @@
 import 'package:flutter/foundation.dart';
 
 class Config {
-  // Use http://127.0.0.1:8000 for web/desktop.
-  // Use http://10.0.2.2:8000 for Android emulator.
-  // Use your computer's local IP (e.g. http://192.168.1.46:8000) for a physical device.
-  static const String _deviceBaseUrl = 'http://192.168.1.67:8000';
+  static const String _productionUrl = 'http://abdullahgilani.pythonanywhere.com';
 
   static String get baseUrl {
-    if (kIsWeb) {
-      return 'http://127.0.0.1:8000';
-    }
-    return _deviceBaseUrl;
+    return _productionUrl;
   }
+
 }
